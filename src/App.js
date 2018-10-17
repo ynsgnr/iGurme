e5155ec8f1fe3395b3cc3d819417667226e1afc4
Yunus <yunusgnr@gmail.com>
2018-10-17 11:19:31 +0300

    Added vector icons for android

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -24,6 +26,7 @@ export default class App extends Component<Props> {
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <FontAwesome5 name={'comments'} solid />
       </View>
     );
   }
