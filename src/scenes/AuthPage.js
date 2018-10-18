4704f5a2bc34e1211c56990523d6870d6f89f2a3
Yunus <yunusgnr@gmail.com>
2018-10-18 22:33:38 +0300

    Added login flow for cart page

--- src/scenes/AuthPage.js
+++ src/scenes/AuthPage.js
@@ -1,12 +1,28 @@
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TouchableHighlight, TextInput} from 'react-native';
+import {Platform, StyleSheet, Text, View, TouchableHighlight, TextInput, ImageBackground} from 'react-native';
+
+import {Auth} from '../actions'
+
+import {LoginBoxy, LoginSoftly} from '../components'
 
 class AuthPage extends Component<Props> {
+
   render() {
+    const layoutDesign = 2
+    const resizeMode = 'center'
     return (
-      <Text>AuthPage</Text>
-    );
+      <ImageBackground style={{flex:1,width:'100%',height:'100%',position:'absolute',justifyContent:'center'}}
+        source={{uri:'https://firebasestorage.googleapis.com/v0/b/igurme-9b5a0.appspot.com/o/Splash.png?alt=media&token=808335a6-6ad7-42b5-a36c-251a9d9a0567'}}
+     >
+       <Auth>{(auth)=>{
+          if(layoutDesign==1) return (<LoginBoxy onLogin={(username,password)=>{auth.login(username,password)}}/>)
+          return(<LoginSoftly onLogin={(username,password)=>{auth.login(username,password)}}/>)
+         }
+       }
+       </Auth>
+     </ImageBackground>
+    )
   }
 }
 
--- src/scenes/Cart.js
+++ src/scenes/Cart.js
@@ -1,11 +1,34 @@
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TouchableHighlight, TextInput} from 'react-native';
+import {Platform, StyleSheet, Text, View, TouchableOpacity, TextInput, ActivityIndicator} from 'react-native';
+
+import {Auth} from '../actions'
+
+import {colors} from '../resources'
 
 class Cart extends Component<Props> {
+
   render() {
     return (
-      <Text>Cart</Text>
+      <Auth>{(auth)=>{
+          if(auth.isLogedIn){return (
+              <Text>Cart</Text>
+          )}
+          this.props.navigation.navigate('AuthPage')
+          return(
+            <View style={{flex:1,width:'100%',height:'100%',alignItems:'center',justifyContent:'center'}}>
+              <Text style={{padding:'5%'}}>Login is required for this page</Text>
+              <TouchableOpacity style={{backgroundColor:colors.mainColor,padding:'3%',borderRadius:20}}
+                onPress={()=>this.props.navigation.navigate('AuthPage')}>
+                <Text style={{color:colors.whiteText,fontWeight: 'bold',paddingLeft:'3%',paddingRight:'3%'}}>
+                  Press Here to Login
+                </Text>
+              </TouchableOpacity>
+            </View>
+          )
+        }
+      }
+      </Auth>
     );
   }
 }
