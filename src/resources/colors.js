6791dfc2a20381fbe30927288c6d2fb4fb436ce2
Yunus <yunusgnr@gmail.com>
2018-10-18 19:11:59 +0300

    Added colors from design

---
+++ src/resources/colors.js
@@ -0,0 +1,14 @@
+const colors = {
+  background: "rgb(224,224,224)",
+  contrast:"rgb(140,160,179)",
+  itemBackground:"rgb(225,225,225)",
+  contrastDark:"rgb(238,238,238)",
+  contrastLight:"rgb(196,211,217)",
+  contrastGray:"rgb(223,223,223)",
+  mainColor:"rgb(245,166,34)",
+  darkGreen:"rgb(65,117,5)",
+  darkText:"rgb(74,74,74)",
+  lightText:"rgb(159,159,159)",
+}
+
+export {colors}
