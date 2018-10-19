b8a70b9fe6d31c2ca6538255c1becb109e900a1d
Yunus <yunusgnr@gmail.com>
2018-10-19 11:00:31 +0300

    Styling login layout to fit better

--- src/scenes/AuthPage.js
+++ src/scenes/AuthPage.js
@@ -9,15 +9,15 @@ import {LoginBoxy, LoginSoftly} from '../components'
 class AuthPage extends Component<Props> {
 
   render() {
-    const layoutDesign = 2
+    const layoutDesign = 1
     const resizeMode = 'center'
     return (
-      <ImageBackground style={{flex:1,width:'100%',height:'100%',position:'absolute',justifyContent:'center'}}
+      <ImageBackground style={{flex:1,paddingTop:'5%',width:'100%',height:'100%',position:'absolute',justifyContent:'flex-start',flexDirection:'column'}}
         source={{uri:'https://firebasestorage.googleapis.com/v0/b/igurme-9b5a0.appspot.com/o/Splash.png?alt=media&token=808335a6-6ad7-42b5-a36c-251a9d9a0567'}}
      >
        <Auth>{(auth)=>{
-          if(layoutDesign==1) return (<LoginBoxy onLogin={(username,password)=>{auth.login(username,password)}}/>)
-          return(<LoginSoftly onLogin={(username,password)=>{auth.login(username,password)}}/>)
+          if(layoutDesign==1) return (<LoginBoxy onLogin={(username,password)=>{auth.login(username,password);this.props.navigation.pop()}}/>)
+          return(<LoginSoftly onLogin={(username,password)=>{auth.login(username,password);this.props.navigation.pop()}}/>)
          }
        }
        </Auth>
