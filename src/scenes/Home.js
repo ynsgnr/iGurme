dae05a4c9bb63e71326be785b346c12977cf40c1
Yunus <yunusgnr@gmail.com>
2018-10-20 15:45:30 +0300

    Added productswipers to homescreen

--- src/scenes/Home.js
+++ src/scenes/Home.js
@@ -6,6 +6,8 @@ import {getColor} from '../resources'
 
 import {Product} from '../actions'
 
+import {ProductSwiper} from '../components'
+
 class Home extends Component<Props> {
   render() {
     return (
@@ -20,12 +22,24 @@ class Home extends Component<Props> {
             )
           }
           return (
-            <View>
-              <ScrollView style={{flexDirection:'row', height:'40%'}} horizontal showsHorizontalScrollIndicator pagingEnabled>
+            <ScrollView style={{backgroundColor:getColor('white')}}>
+              <ScrollView style={{flexDirection:'row', height:Dimensions.get("window").height*0.3}} horizontal showsHorizontalScrollIndicator pagingEnabled>
                 {imageObjects}
               </ScrollView>
-              <Text>Test</Text>
-            </View>
+              <View style={{paddingTop:'10%'}}>
+                <View style={{paddingTop:'5%',paddingLeft:'5%',paddingRight:'5%',flexDirection:'row',alignItems:'center',justifyContent:'space-between',}}>
+                   <Text style={{color:getColor('darkText'),fontSize:25,fontWeight:'bold'}}>BEST SELLERS</Text>
+                   <Text style={{color:getColor('lightText'),fontSize:17}}>See All</Text>
+                </View>
+                <ProductSwiper style={{height:Dimensions.get("window").height*0.35}} data={products.bestSellers}/>
+                <View style={{paddingTop:'5%',paddingLeft:'5%',paddingRight:'5%',flexDirection:'row',alignItems:'center',justifyContent:'space-between',}}>
+                   <Text style={{color:getColor('darkText'),fontSize:25,fontWeight:'bold'}}>NEW</Text>
+                   <Text style={{color:getColor('lightText'),fontSize:17}}>See All</Text>
+                </View>
+                <ProductSwiper style={{height:Dimensions.get("window").height*0.35}} data={products.new}/>
+
+              </View>
+            </ScrollView>
           )}
         }</Product>
       </View>
