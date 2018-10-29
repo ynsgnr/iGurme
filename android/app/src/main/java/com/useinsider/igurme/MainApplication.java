4f0f90fe19fb810d46dc2bd45bd8d8d4d9c84a88
Yunus <yunusgnr@gmail.com>
2018-10-29 19:40:00 +0300

    Removed firebase from android

--- android/app/src/main/java/com/useinsider/igurme/MainApplication.java
+++ android/app/src/main/java/com/useinsider/igurme/MainApplication.java
@@ -8,10 +8,6 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
-import io.invertase.firebase.RNFirebasePackage;
-import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
-import io.invertase.firebase.auth.RNFirebaseAuthPackage;
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -26,10 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-          new RNFirebasePackage(),
-          new RNFirebaseFirestorePackage(),
-          new RNFirebaseAuthPackage()
+          new MainReactPackage()
       );
     }
 
